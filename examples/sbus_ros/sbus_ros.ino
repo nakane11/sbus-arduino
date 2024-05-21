1a0ecc063b2f3d4654dee9ee0de84a750ae0a2b0
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2024-05-21 13:56:47 +0900

    Fix msg type

--- examples/sbus_ros/sbus_ros.ino
+++ examples/sbus_ros/sbus_ros.ino
@@ -6,29 +6,29 @@
 /************************************************************/
 #include <BluetoothHardware.h>
 #include <ros.h>
-#include <std_msgs/UInt8MultiArray.h>
+#include <std_msgs/Float32MultiArray.h>
 
 #define SBUS_SPEED    100000     //SBUS通信速度（9600 or 100000）
 
 ros::NodeHandle_<BluetoothHardware> nh;
-void cb(const std_msgs::UInt8MultiArray& sub_msg){
-  nh.loginfo("cb"); 
+void cb(const std_msgs::Float32MultiArray& sub_msg){
+  nh.loginfo("cb");
   for(int i=0;i<6;i++){
-    //setServoAngle(i, sub_msg.data[i]);
-    setServoAngle(i, 90);
+    setServoAngle(i, sub_msg.data[i]);
   }
   sendSbusData();
+  delay(10);
 }
-ros::Subscriber<std_msgs::UInt8MultiArray> sub("servo_states", cb);
+ros::Subscriber<std_msgs::Float32MultiArray> sub("servo_states", cb);
 
-uint8_t sbus_data[25] = {
+char sbus_data[25] = {
   0x0f, 0x00, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x00, 0x00
 };
-uint8_t sbus_servo_id[16];                            //送信用指令角度格納配列（SBUS用変換後）
+short  sbus_servo_id[16];                            //送信用指令角度格納配列（SBUS用変換後）
 
 void setup() {
   Serial.begin(SBUS_SPEED, SERIAL_8E2); // 100kbps 1データ8bit　パリティEven StopBit:2bit
@@ -59,7 +59,7 @@ void sendSbusData(void){
     Serial.write(sbus_data, 25); 
 }
 
-void setServoAngle(uint8_t id, float angle) {
+void setServoAngle(int id, float angle) {
   if (id < 0 || id >= 16) return; // IDが範囲外の場合は何もしない
   sbus_servo_id[id] = (int)(  10.667 * (double)(angle + 90.0) + 64);
 }
